16c5c1eb66cf958861dfbf6503590ca7ed97c15d
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2023-10-20 16:36:29 +0300

    begin adding chroot commands

---
+++ pikainstall/usr/lib/pika/pikainstall/pika-install-host.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+ mkdir -pv "$1"/media/cdrom
+ mount --bind /cdrom "$1"/media/cdrom
+ mount --rbind /dev "$1"/dev
+ mount --rbind /dev/pts "$1"/dev/pts
+ mount --rbind /proc "$1"/proc
+ mount --rbind /sys "$1"/sys
+ mount --rbind /run "$1"/run
+ mkdir -p "$1"/var/cache/apt/archives
+ cp -rvf /cdrom/pool/main/* "$1"/var/cache/apt/archives/
+ chroot /chroot_dir /bin/bash -c "su - -c /usr/lib/pika/pikainstall/pika-install-chroot.sh"
