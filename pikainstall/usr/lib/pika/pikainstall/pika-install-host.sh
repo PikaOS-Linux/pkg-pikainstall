5e1db17c190d3599d8c5b5365db8b645c6bfb5e4
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2023-10-20 19:55:11 +0300

    wtf

--- pikainstall/usr/lib/pika/pikainstall/pika-install-host.sh
+++ pikainstall/usr/lib/pika/pikainstall/pika-install-host.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
- mkdir -pv "$1"/media/cdrom
- mount --bind /cdrom "$1"/media/cdrom
- mount --rbind /dev "$1"/dev
- mount --rbind /dev/pts "$1"/dev/pts
- mount --rbind /proc "$1"/proc
- mount --rbind /sys "$1"/sys
- mount --rbind /run "$1"/run
- rm -rfv "$1"/boot/*arch*
- mkdir -p "$1"/var/cache/apt/archives
- cp -rvf /cdrom/pool/main/* "$1"/var/cache/apt/archives/
- genfstab -U "$1" > "$1"/etc/fstab
- cat "$1"/etc/fstab | grep -v zram > "$1"/etc/fstab
- chroot "$1" /bin/bash -c "su - -c /usr/lib/pika/pikainstall/pika-install-chroot.sh"
+mkdir -pv "$1"/media/cdrom
+mount --bind /cdrom "$1"/media/cdrom
+mount --rbind /dev "$1"/dev
+mount --rbind /dev/pts "$1"/dev/pts
+mount --rbind /proc "$1"/proc
+mount --rbind /sys "$1"/sys
+mount --rbind /run "$1"/run
+rm -rfv "$1"/boot/*arch*
+mkdir -p "$1"/var/cache/apt/archives
+cp -rvf /cdrom/pool/main/* "$1"/var/cache/apt/archives/
+genfstab -U "$1" > "$1"/etc/fstab
+cat "$1"/etc/fstab | grep -v zram > "$1"/etc/fstab
+chroot "$1" /bin/bash -c "su - -c /usr/lib/pika/pikainstall/pika-install-chroot.sh"
